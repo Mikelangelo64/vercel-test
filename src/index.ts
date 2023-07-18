4c10e7e4e3eb717a125631128e3c6700cb56bd89
Mikelangelo64 <donni2001331@gmail.com>
2023-07-18 13:44:53 +0300

    test preloader

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-// import { Preloader } from 'vevet';
+import { Preloader } from 'vevet';
 import { init } from '@/scripts/main';
 import '@/styles/styles.scss';
 import vevet from './scripts/config/vevet';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   init();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+});
 // eslint-disable-next-line no-new
-// new Preloader({
-//   container: '#v-preloader',
-//   hide: 300,
-// });
+new Preloader({
+  container: '#v-preloader',
+  hide: 300,
+});
 
 vevet.pageLoad.onLoaded(() => {
   init();
